94916ba61a5b0b53b3169647704c17092185f057
Marco Lombardini <marco.lombardini@gmail.com>
2019-04-25 11:53:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ def batch_badge_creator(speakers)
 end
 
 def assign_rooms(speakers)
-  speakers.each_with_index.map do |speaker, index|
+  speakers.each_with_index.collect do |speaker, index|
     "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
   end
 end
